485f66c9bcbe4c735dd2cbe0f9dbc78ec503cff8
안동대 백수 <79616817+sasileunnadojalmorem@users.noreply.github.com>
2024-08-28 21:13:12 +0900

    dd

--- oshi-front/src/App.tsx
+++ oshi-front/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
     return (
       <Router>
       <Routes>
-      <Route path={MAIN_PATH( )} element={<footer/>}/>
+      <Route path={MAIN_PATH( )} element={<Header/>}/>
 
       </Routes>
       {/* Container 컴포넌트를 사용하여 헤더, 푸터와 함께 페이지 렌더 */}
